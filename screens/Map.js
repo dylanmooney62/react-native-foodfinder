ecd7565d352f20008ec83ad5e9b909b9d4422603
Dylan Mooney <33304160+dylanmooney62@users.noreply.github.com>
2021-10-17 17:10:31 +0100

    added map

--- screens/Map.js
+++ screens/Map.js
@@ -2,16 +2,52 @@ import React, { useContext } from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { LocationContext } from '../context/LocationContext';
+import { useRestaurants } from '../hooks/useRestaurants';
+import { Marker } from 'react-native-maps';
 
 const Map = () => {
-  const { lat, lng } = useContext(LocationContext);
+  const { location } = useContext(LocationContext);
+
+  const latitude = location?.latitude;
+  const longitude = location?.longitude;
+
+  const { restaurants, loading, error } = useRestaurants(
+    latitude,
+    longitude,
+    1500
+  );
+
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (error) {
+    return <Text>Something went wrong...</Text>;
+  }
 
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        initialRegion={{ latitude: lat, longitude: lng }}
-      />
+        region={{
+          latitude,
+          longitude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      >
+        {restaurants.map(({ name, vicinity, geometry: { location } }, idx) => (
+          <Marker
+            key={idx}
+            coordinate={{
+              latitude: location.lat,
+              longitude: location.lng,
+            }}
+            title={name}
+            description={vicinity}
+          />
+        ))}
+      </MapView>
     </View>
   );
 };
@@ -27,6 +63,6 @@ const styles = StyleSheet.create({
   },
   map: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    height: Dimensions.get('window').height - 64,
   },
 });
